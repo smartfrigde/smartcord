0780fd928cc33195b69a7ecbd104fe5ddb3a0d9e
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2021-03-17 12:20:18 +0100

    Fixed reload/restart bug

--- injection.js
+++ injection.js
@@ -2,7 +2,7 @@ require('./main_process_shit');
 const electron = require('electron');
 const path = require('path');
 electron.app.commandLine.appendSwitch("no-force-async-hooks-checks");
-
+global.onerror = null;
 electron.session.defaultSession.webRequest.onHeadersReceived(function(details, callback) {
     if (!details.responseHeaders['content-security-policy-report-only'] && !details.responseHeaders['content-security-policy']) return callback({cancel: false});
     delete details.responseHeaders['content-security-policy-report-only'];
