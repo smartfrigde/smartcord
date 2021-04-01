94199c2ce6f61edbeb80adb4569cb3aa4dafd688
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2021-04-01 17:38:34 +0200

    Add CSP Remover Back for Windows

--- injection.js
+++ injection.js
@@ -3,7 +3,12 @@ const electron = require('electron');
 const path = require('path');
 electron.app.commandLine.appendSwitch("no-force-async-hooks-checks");
 global.onerror = null;
-
+electron.session.defaultSession.webRequest.onHeadersReceived(function(details, callback) {
+    if (!details.responseHeaders['content-security-policy-report-only'] && !details.responseHeaders['content-security-policy']) return callback({cancel: false});
+    delete details.responseHeaders['content-security-policy-report-only'];
+    delete details.responseHeaders['content-security-policy'];
+    callback({cancel: false, responseHeaders: details.responseHeaders});
+});
 class BrowserWindow extends electron.BrowserWindow {
     constructor(originalOptions) {
         let win = new electron.BrowserWindow(originalOptions);
