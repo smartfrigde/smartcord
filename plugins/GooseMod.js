5ed4726672bdd59c397f40be5998ae5a6b84c657
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2021-04-13 08:30:57 +0200

    Update GooseMod.js

--- plugins/GooseMod.js
+++ plugins/GooseMod.js
@@ -1,12 +1,12 @@
 const Plugin = require("../plugin");
 
 module.exports = new Plugin({
-  name: "GooseMod Plugin Loader" /* Human-readable plugin name. */,
+  name: "GooseMod Loader" /* Human-readable plugin name. */,
   author:
     "smartfridge#0001" /* [Optional] Put your name here to give yourself credit for making it :) */,
   description:
-    "Loads GooseMod Plugins" /* Description of what this plugin does. */,
-  preload: true /* [Optional] If true, load this before Discord has finished starting up */,
+    "Loads GooseMod Plugins and Themes" /* Description of what this plugin does. */,
+  preload: true  /* [Optional] If true, load this before Discord has finished starting up */,
   color:
     "#646" /* [Optional] The color that this plugin shows in logs and in the plugin settings tab. Any valid CSS color will work here. */,
   disabledByDefault: true /* [Optional] If true, disable the plugin until the user enables it in settings */,
@@ -21,5 +21,6 @@ module.exports = new Plugin({
   },
   unload: function () {
     /* What your plugin does when it is disabled or being reloaded. */
+    location.reload();
   },
 });
