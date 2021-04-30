c121012cac849bd5dba872d2ad22de00cbb36164
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2021-04-30 18:03:34 +0200

    Remove perms.js as it's not needed anymore

--- core/perms.js
+++
@@ -1,7 +0,0 @@
-class SCPerms {
-    static checkPerms(pluginID, perms){
-        SCApi.alert("SmartCord Permissions", `Are you sure you want to run ${pluginID}? It uses following perms: ${perms}`)
-    }
-};
-
-module.exports = SCPerms;
